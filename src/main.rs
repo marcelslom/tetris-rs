b18b47d8a8a68f97b0aa9a20121f6d167a14ac51
marcelslom <95636743+marcelslom@users.noreply.github.com>
2024-02-07 20:53:41 +0100

    Partial physics implementation

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@ use crossterm::{
     ExecutableCommand,
 };
 use ratatui::{
-    layout::{Constraint, Offset}, prelude::{CrosstermBackend, Frame, Stylize, Terminal}, style::{Color, Style}, text::Text, widgets::{Cell, Paragraph, Row, Table}
+    layout::Constraint,
+    prelude::{CrosstermBackend, Frame, Terminal},
+    style::{Color, Style},
+    text::Text,
+    widgets::{Cell, Row, Table},
 };
 use rusttype::Point;
-use std::io::{stdout, Result};
+use std::{any::Any, io::{stdout, Result}};
+use std::time::{Duration, Instant};
+use std::thread;
 
 const BOARD_WIDTH: usize = 10;
 const BOARD_HEIGHT: usize = 20;
 const NUMBER_OF_TILES: usize = BOARD_WIDTH * BOARD_HEIGHT;
 const TILE_SIZE: u16 = 2;
+const EVENT_POLL_DURATION_MS: u64 = 1;
+const FRAME_DURATION: Duration = Duration::from_micros(16776);
 
-/*#[derive(Clone, Copy)]
-enum Color {
-    Black, White, Yellow, Blue, Purple, Red, Green, Orange, Cyan
-}*/
-
+#[derive(Clone, Copy)]
 enum Tetromino {
     I,
     O,
@@ -27,6 +31,38 @@ enum Tetromino {
     Z,
     J,
     L,
+}
+
+#[derive(Clone, Copy)]
+enum Action {
+    None,
+    SoftDrop,
+    HardDrop,
+    Hold,
+    Left,
+    Right,
+    RotateClockwise,
+    RotateCounterClockwise,
+    CloseGame,
+}
+
+#[derive(Clone, Copy)]
+enum Gravity {
+    None,
+    Normal,
+    SoftDrop,
+    HardDrop
+}
+
+impl Gravity {
+    fn value(&self) -> f32 {
+        match self {
+            Gravity::None => 0f32,
+            Gravity::Normal => 1f32 / 64f32,
+            Gravity::SoftDrop => todo!(),
+            Gravity::HardDrop => todo!(),
+        }
+    }
 }
 
 impl Tetromino {
@@ -53,6 +89,18 @@ impl Tetromino {
             Tetromino::L => vec![vec![false, false, true], vec![true, true, true]],
         }
     }
+
+    fn width(&self) -> usize {
+        match self {
+            Tetromino::I => 4,
+            Tetromino::O => 2,
+            Tetromino::S => 3,
+            Tetromino::Z => 3,
+            Tetromino::T => 3,
+            Tetromino::J => 3,
+            Tetromino::L => 3,
+        }
+    }
 }
 
 struct AppContext {
@@ -63,7 +111,8 @@ struct GameState {
     board: [Color; NUMBER_OF_TILES],
     current_tetromino: Tetromino,
     tetromino_position: Point<usize>,
-    tetromino_set_to_board: bool,
+    tetromino_finished: bool,
+    cumulated_gravity: f32
 }
 
 fn main() -> Result<()> {
@@ -83,45 +132,98 @@ fn game_loop() -> Result<()> {
     let mut game_state = GameState {
         board: [Color::Black; NUMBER_OF_TILES],
         current_tetromino: Tetromino::I,
-        tetromino_position: Point {
-            x: 0,
-            y: 0,
-        },
-        tetromino_set_to_board: false,
+        tetromino_position: Point { x: 0, y: 0 },
+        tetromino_finished: false,
+        cumulated_gravity: 0f32
     };
 
     loop {
-        game_logic(&mut game_state, &mut app_context)?;
-        terminal.draw(|frame| draw(&mut game_state, frame))?;
+        let frame_start = Instant::now();
+        let input = read_input()?;
+        app_logic(&mut app_context, input);
         if app_context.should_close {
             break;
+        }
+        game_logic(&mut game_state, input);
+        terminal.draw(|frame| draw(&mut game_state, frame))?;
+        let elapsed = frame_start.elapsed();
+        if elapsed < FRAME_DURATION {
+            let time_to_wait = FRAME_DURATION - elapsed;
+            thread::sleep(time_to_wait);
         }
     }
 
     Ok(())
 }
 
-fn game_logic(game: &mut GameState, app_context: &mut AppContext) -> Result<()> {
-    if event::poll(std::time::Duration::from_millis(250)).unwrap() {
-        if let event::Event::Key(key) = event::read().unwrap() {
-            if key.kind == KeyEventKind::Press{
-                match key.code {
-                    KeyCode::Char('q') => app_context.should_close = true,
-                    _ => {}
-                }
+fn read_input() -> Result<Action> {
+    if event::poll(std::time::Duration::from_millis(EVENT_POLL_DURATION_MS))? {
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                return match key.code {
+                    KeyCode::Char('q') => Ok(Action::CloseGame),
+                    KeyCode::Up => Ok(Action::RotateClockwise),
+                    KeyCode::Char('0')=> Ok(Action::RotateCounterClockwise),
+                    KeyCode::Down => Ok(Action::SoftDrop),
+                    KeyCode::Char(' ') => Ok(Action::HardDrop),
+                    KeyCode::Char('c') => Ok(Action::Hold),
+                    KeyCode::Left => Ok(Action::Left),
+                    KeyCode::Right => Ok(Action::Right),
+                    _ => Ok(Action::None),
+                };
             }
         }
     }
-    Ok(())
+    Ok(Action::None)
+}
+
+fn app_logic(context: &mut AppContext, input: Action) {
+    if matches!(input, Action::CloseGame) {
+        context.should_close = true;
+    }
+}
+
+fn game_logic(game: &mut GameState, input: Action) {
+    match input {
+        Action::None => game.cumulated_gravity += Gravity::Normal.value(),
+        Action::SoftDrop => game.cumulated_gravity = Gravity::SoftDrop.value(),
+        Action::HardDrop => game.cumulated_gravity = Gravity::HardDrop.value(),
+        Action::Hold => return,
+        Action::Left => {
+            game.cumulated_gravity += Gravity::Normal.value();
+            if game.tetromino_position.x > 0 {
+                game.tetromino_position.x -= 1;
+            }
+        },
+        Action::Right => {
+            game.cumulated_gravity += Gravity::Normal.value();
+            if game.tetromino_position.x < BOARD_WIDTH - game.current_tetromino.width() {
+                game.tetromino_position.x += 1;
+            }
+        },
+        Action::RotateClockwise => todo!(),
+        Action::RotateCounterClockwise => todo!(),
+        Action::CloseGame => {},
+    }
+
+    if game.cumulated_gravity > 1f32 { //move tetromino down
+        while game.cumulated_gravity > 1f32 {
+            //todo check collision
+            game.tetromino_position.y += 1;
+            game.cumulated_gravity -= 1f32;
+        }
+        game.cumulated_gravity = 0f32; // reset gravity to avoid errors related to the cumulation of fractional parts.
+    }
+
 }
 
 //this is TUI for test purposes, probably will be reimplemented later
 fn draw(game: &mut GameState, frame: &mut Frame) {
     let mut table_colors = game.board.clone();
-    if !game.tetromino_set_to_board {
+    if !game.tetromino_finished {
         let mut y_offset = game.tetromino_position.y * BOARD_WIDTH;
         for line in game.current_tetromino.shape() {
-            let mut x_offset =  game.tetromino_position.x;
+            let mut x_offset = game.tetromino_position.x;
             for value in line {
                 if value {
                     table_colors[y_offset + x_offset] = game.current_tetromino.color();
@@ -134,13 +236,15 @@ fn draw(game: &mut GameState, frame: &mut Frame) {
 
     let widths = vec![Constraint::Length(TILE_SIZE); BOARD_WIDTH];
     let rows = table_colors.chunks(BOARD_WIDTH).map(|row| {
-        Row::new(row.iter().map(|cell| {
-            Cell::from(Text::from("")).style(Style::default().bg(*cell))
-        }))
+        Row::new(
+            row.iter()
+                .map(|cell| Cell::from(Text::from("")).style(Style::default().bg(*cell))),
+        )
         .height(TILE_SIZE)
     });
     let table = Table::new(rows, widths).column_spacing(0);
-    frame.render_widget(table, frame.size())
+
+    frame.render_widget(table, frame.size());
 }
 
 fn startup() -> Result<()> {
